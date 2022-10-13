01d2da031bfcc4da5a0ea5fe92bb4d265e8ff10f
alifananta255 <alifananta255@gmail.com>
2022-10-13 07:25:34 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+    class Contoh1 extends CI_Controller {
+        public function index() {
+            echo "<h1>Perkenalan</h1>";
+            echo "Nama saya Alif Ananta Ulya
+                  Saya tinggal di daerah Kemayoran
+                  olah raga yang saya sukai adalah
+                  Sepakbola";
+        }
+    }
+?>
---
+++ application/views/contoh1.php
@@ -0,0 +1,3 @@
+<h1>Perkenalkan</h1>
+Nama saya Alif Ananta Ulya
+ Saya tinggal di Utan Panjang
